0cbe31bf467679e98a89fbb70e6a40097e95006d
Mark Brough <mark.brough@gmail.com>
2024-06-11 20:55:32 +0200

    Update format of slug pages

---
+++ pages/[slug].vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <ContentQuery :path="localizedPath($route.path)" find="one" v-slot="{ data }">
+      <template v-if="data?.title">
+        <Head>
+          <Title>{{ data.title }} | {{config.public.title}}</Title>
+          <Meta name="twitter:title" :content="`${data.title} | ${config.public.title}`" />
+          <Meta name="og:title" :content="`${data.title} | ${config.public.title}`" />
+        </Head>
+        <ContentRenderer :value="data" />
+      </template>
+    </ContentQuery>
+  </div>
+</template>
+<script setup>
+
+const i18n = useI18n();
+function localizedPath(path) {
+  // Use this.$i18n.locale to get the current locale
+  const locale = i18n.locale.value;
+  // Add the locale to the path
+  if (path.includes(locale)) {
+    return path
+  } else {
+    return `/${locale}${path}`;
+  }
+}
+const config = useRuntimeConfig()
+</script>
