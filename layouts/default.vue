4383fa0d52108650a54ae15818d8139837ab7bcd
Mark Brough <mark@brough.io>
2025-12-05 18:57:46 +0100

    Linting

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,8 @@
 <template>
   <div>
+
     <Head>
-      <Title>{{config.public.title}}</Title>
+      <Title>{{ config.public.title }}</Title>
       <Meta name="twitter:title" :content="config.public.title" />
       <Meta name="og:title" :content="config.public.title" />
       <Meta name="twitter:image" :content="config.public.twitterImage" />
@@ -24,37 +25,30 @@
           </div>
         </b-container>
       </div>
-      <b-navbar toggleable="md"  v-b-color-mode="'dark'" variant="dark" class="primary-navbar">
-          <b-container>
-        <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
-        <b-collapse id="nav-collapse" is-nav>
+      <b-navbar toggleable="md" v-b-color-mode="'dark'" variant="dark" class="primary-navbar">
+        <b-container>
+          <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+          <b-collapse id="nav-collapse" is-nav>
             <b-navbar-nav>
-              <b-nav-item
-                :to="localePath($t('menuItems.whatWeDo.value'))"
-                class="me-lg-3 fs-5"><font-awesome :icon="['fas', 'house']" class="me-2" /> {{ $t('menuItems.whatWeDo.text') }}</b-nav-item>
-              <b-nav-item
-                :to="localePath($t('menuItems.whoWeAre.value'))"
-                class="me-lg-3 fs-5">{{ $t('menuItems.whoWeAre.text') }}</b-nav-item>
-              <b-nav-item
-                :to="localePath($t('menuItems.getInTouch.value'))"
-                class="me-lg-3 fs-5">{{ $t('menuItems.getInTouch.text') }}</b-nav-item>
-              <b-nav-item
-                :to="localePath($t('menuItems.blog.value'))"
-                class="me-lg-3 fs-5">{{ $t('menuItems.blog.text') }}</b-nav-item>
+              <b-nav-item :to="localePath($t('menuItems.whatWeDo.value'))" class="me-lg-3 fs-5"><font-awesome
+                  :icon="['fas', 'house']" class="me-2" /> {{ $t('menuItems.whatWeDo.text') }}</b-nav-item>
+              <b-nav-item :to="localePath($t('menuItems.whoWeAre.value'))" class="me-lg-3 fs-5">{{
+                $t('menuItems.whoWeAre.text') }}</b-nav-item>
+              <b-nav-item :to="localePath($t('menuItems.getInTouch.value'))" class="me-lg-3 fs-5">{{
+                $t('menuItems.getInTouch.text') }}</b-nav-item>
+              <b-nav-item :to="localePath($t('menuItems.blog.value'))" class="me-lg-3 fs-5">{{ $t('menuItems.blog.text')
+              }}</b-nav-item>
             </b-navbar-nav>
             <b-navbar-nav class="ms-auto mb-2 mb-lg-0 fs-5">
               <b-nav-item-dropdown :text="$t('selectLanguage')" right id="language">
-                <b-dropdown-item
-                  v-for="locale in availableLocales"
-                  v-bind:key="locale.code"
-                  :to="switchLocalePath(locale.code)"
-                  :active="$i18n.locale == locale.code">
+                <b-dropdown-item v-for="locale in availableLocales" v-bind:key="locale.code"
+                  :to="switchLocalePath(locale.code)" :active="$i18n.locale == locale.code">
                   {{ locale.name }}
                 </b-dropdown-item>
               </b-nav-item-dropdown>
             </b-navbar-nav>
-        </b-collapse>
-          </b-container>
+          </b-collapse>
+        </b-container>
       </b-navbar>
       <b-container class="pt-md-5 pt-3 p-3 page-body">
         <slot />
@@ -66,9 +60,10 @@
           <b-row>
             <b-col>Copyright &copy; Emergentally GmbH. All rights reserved.</b-col>
             <b-col class="text-end fs-4">
-              <nuxt-link href="https://mastodon.social/@emergentally" class="me-3"><font-awesome :icon="['fab', 'mastodon']" /></nuxt-link>
-              <nuxt-link href="https://bsky.app/profile/emergentally.bsky.social" class="me-3"><font-awesome :icon="['fab', 'bluesky']" /></nuxt-link>
-              <nuxt-link href="https://x.com/emergentally" class="me-3"><font-awesome :icon="['fab', 'twitter']" /></nuxt-link>
+              <nuxt-link href="https://mastodon.social/@emergentally" class="me-3"><font-awesome
+                  :icon="['fab', 'mastodon']" /></nuxt-link>
+              <nuxt-link href="https://bsky.app/profile/emergentally.bsky.social" class="me-3"><font-awesome
+                  :icon="['fab', 'bluesky']" /></nuxt-link>
             </b-col>
           </b-row>
         </footer>
@@ -80,9 +75,11 @@
 a.nav-link {
   border-bottom: 2px solid transparent;
 }
+
 a.nav-link:hover {
   border-bottom: 2px solid grey;
 }
+
 hr {
   margin-top: 40px;
 }
@@ -94,14 +91,23 @@ h1 {
 
 /* The typewriter cursor effect */
 @keyframes blink-caret {
-  from, to { border-color: transparent }
-  50% { border-color: white; }
+
+  from,
+  to {
+    border-color: transparent
+  }
+
+  50% {
+    border-color: white;
+  }
 }
+
 .blog-article img {
   box-shadow: 4px 4px 10px #000000;
   max-width: calc(min(350px, 100%));
   margin-bottom: 10px;
 }
+
 .card-deck img {
   box-shadow: initial;
   margin: initial;
@@ -110,24 +116,32 @@ h1 {
 .header-container {
   width: 100%;
 }
+
 @font-face {
   font-family: tangoSans;
   src: url(/tangosans/TangoSans.ttf);
 }
+
 .primary-navbar {
   border-bottom: 1px solid #dddddd;
 }
+
 .primary-navbar.bg-dark {
   background-color: #000000 !important;
 }
-.primary-navbar.bg-dark a, .primary-navbar.bg-dark button {
+
+.primary-navbar.bg-dark a,
+.primary-navbar.bg-dark button {
   color: #ffffff !important;
 }
+
 .w-33 {
   width: 33%;
 }
+
 .jumbotron {
-  background: none; /* background:  url('~/static/noun-data-5139476.svg') no-repeat right top fixed; */
+  background: none;
+  /* background:  url('~/static/noun-data-5139476.svg') no-repeat right top fixed; */
   -webkit-background-size: 600px 600px;
   -moz-background-size: 600px 600px;
   -o-background-size: 600px 600px;
@@ -137,18 +151,20 @@ h1 {
   padding-bottom: 1rem !important;
   transition: all 1s;
 }
+
 .jumbotron .lead {
   visibility: hidden;
   opacity: 0;
   height: 0px;
   padding: 0px;
   margin: 0px;
-  font-family:  tangoSans;
+  font-family: tangoSans;
   color: #ffffff;
   text-shadow: 0px 0px 1px #ffffff;
   font-size: 1.7em;
   background: #000000;
-  border-right: .15em solid orange; /* The typwriter cursor */
+  border-right: .15em solid orange;
+  /* The typwriter cursor */
   animation:
     blink-caret .75s step-end infinite;
   transition: visibility 0s, opacity 0.5s linear;
@@ -163,18 +179,20 @@ h1 {
   -o-background-size: cover;
   background-size: cover;
   */
-  background: linear-gradient(45deg,#448822,#666) repeat scroll 0 0 transparent;
-  filter: progid:DXImageTransform.Microsoft.gradient(startColorstr="#448822",endColorstr="#666666",GradientType=1);
+  background: linear-gradient(45deg, #448822, #666) repeat scroll 0 0 transparent;
+  filter: progid:DXImageTransform.Microsoft.gradient(startColorstr="#448822", endColorstr="#666666", GradientType=1);
 
   box-shadow: inset 0 3px 7px rgb(0 0 0 / 20%), inset 0 -3px 7px rgb(0 0 0 / 20%);
   margin: 0px;
   color: #fff;
   transition: all 1s;
 }
+
 .header-home .jumbotron {
   padding-top: 3rem !important;
   padding-bottom: 3rem !important;
 }
+
 .header-home .jumbotron .lead {
   opacity: 1;
   visibility: visible;
@@ -183,65 +201,80 @@ h1 {
   margin-left: -5px;
   height: auto;
 }
+
 .jumbotron h1 {
   font-family: tangoSans;
-  color:  #ffffff;
+  color: #ffffff;
   margin-top: 5px;
   text-shadow: 0px 0px 20px #444444;
   font-size: 5em !important;
   letter-spacing: -1px;
   transition: all 1s;
 }
+
 .jumbotron a {
   text-decoration: none;
 }
+
 .header-home .jumbotron h1 {
   font-size: 7em;
   margin-top: 20px;
 }
+
 @media (max-width: 1200px) {
   .jumbotron h1 {
     font-size: 3.5em !important;
     letter-spacing: -1px;
   }
+
   .jumbotron .lead {
     font-size: 1.2em;
   }
+
   .header-home .jumbotron h1 {
     font-size: 3.5em;
     letter-spacing: -1px;
   }
+
   .header-home .jumbotron .lead {
     font-size: 1.2em;
   }
 }
+
 @media (max-width: 576px) {
   .jumbotron h1 {
     font-size: 3em !important;
     letter-spacing: -1px;
   }
+
   .jumbotron .lead {
     font-size: 1em;
   }
+
   .header-home .jumbotron {
     padding-top: 2rem !important;
     padding-bottom: 2rem !important;
   }
+
   .header-home .jumbotron h1 {
     font-size: 2.5em;
     letter-spacing: -1px;
   }
+
   .header-home .jumbotron .lead {
     font-size: 1em;
   }
 }
+
 html {
   height: 100%;
 }
+
 /* NB this doesn't take into account the home page, but that is normally OK anyway */
 .page-body {
   margin-bottom: 20px;
-  min-height: calc(100vh - 8.25rem - 6rem - 7rem + 1px); /* 100% - header - navbar - footer */
+  min-height: calc(100vh - 8.25rem - 6rem - 7rem + 1px);
+  /* 100% - header - navbar - footer */
 }
 
 
@@ -249,25 +282,31 @@ html {
   font-family: tangoSans;
   font-size: 4em;
 }
+
 .page-body h2 {
   font-family: tangoSans;
 }
+
 .page-body h2 a {
   color: #000000;
   text-decoration: none;
 }
+
 .page-body p {
   font-size: 1.25rem;
   font-weight: 300;
 }
+
 @media (max-width: 1200px) {
   .page-body h1 {
     font-size: 2.5em;
   }
+
   .page-body h2 {
     font-size: 2em;
   }
 }
+
 .page-body b {
   background-color: #448822;
   color: #ffffff;
@@ -282,8 +321,7 @@ html {
 blockquote {
   border-left: 8px solid grey;
   padding-left: 14px;
-}
-</style>
+}</style>
 <script setup>
 const config = useRuntimeConfig();
 const { locale, locales } = useI18n();
