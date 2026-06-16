920c44f7bc57e2225e5b100d025cf27ad8a8b5d7
Mark Brough <mark@brough.io>
2026-06-16 11:18:59 +0000

    Add updated development dashboard page

---
+++ pages/development-dashboard.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+      <Head>
+        <Title>Development Dashboard | {{config.public.title}}</Title>
+        <Meta name="twitter:title" :content="`${config.public.title}`" />
+        <Meta name="og:title" :content="`${config.public.title}`" />
+      </Head>
+      <ContentDoc v-slot="{doc}" :head="false">
+        <Head>
+          <Title>Development Dashboard | {{config.public.title}}</Title>
+          <Meta name="twitter:title" :content="`${config.public.title}`" />
+          <Meta name="og:title" :content="`${config.public.title}`" />
+        </Head>
+        <e-dd-hero />
+        <e-dd-showcase />
+        <e-dd-narrative />
+        <e-dd-pillars />
+        <e-pull-quote namespace="developmentDashboard.pullQuote" image="none" />
+        <e-dd-features />
+        <e-dd-demo-cta />
+      </ContentDoc>
+  </div>
+</template>
+<script setup>
+const config = useRuntimeConfig()
+</script>
